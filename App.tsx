9a3a1b18822a6e194e92c90056493e62e5be3f03
akellasaikrishna <saikrishna94944@gmail.com>
2020-07-05 13:35:15 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -8,70 +8,90 @@ import {
   Alert,
   TextInput,
   KeyboardAvoidingView,
+  ScrollView,
+  ToastAndroid,
 } from "react-native";
 import { Calendar, CalendarList, Agenda } from "react-native-calendars";
-const dimensions = Dimensions.get("screen");
 interface state {
   items: any;
+  selectedDate: string;
 }
+
+let selectedDate: any;
+const dimensions = Dimensions.get("screen");
 export default class App extends React.Component<any, state> {
   constructor(props: any) {
     super(props);
     this.state = {
       items: {},
+      selectedDate: "",
     };
   }
   render() {
     return (
       <KeyboardAvoidingView
-        style={{ flex: 1, marginTop: 25 }}
+        style={{ flex: 1, marginTop: 35 }}
         behavior="height"
       >
-        <View>
-          <Calendar
-            current={new Date()}
-            onDayPress={(day) => {
-              console.log("selected day", day);
-            }}
-            onDayLongPress={(day) => {
-              console.log("selected day", day);
-            }}
-            onMonthChange={(month) => {
-              console.log("month changed", month);
-            }}
-          />
-        </View>
-        <View
-          style={{
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "#FFFFFF",
-            flex: 1,
-          }}
-        >
-          <Text
+        <ScrollView>
+          <View>
+            <Calendar
+              current={new Date()}
+              onDayPress={(day) => {
+                this.setState({ selectedDate: day["dateString"] });
+              }}
+              onDayLongPress={(day) => {
+                console.log("selected day", day);
+              }}
+              onMonthChange={(month) => {
+                console.log("month changed", month);
+                // this.setState({ notes: "" });
+              }}
+            />
+          </View>
+          <View
             style={{
-              paddingVertical: 10,
-              fontSize: 17,
-              textAlign: "center",
-              fontWeight: "bold",
+              justifyContent: "center",
+              alignItems: "center",
+              backgroundColor: "#FFFFFF",
+              flex: 1,
             }}
           >
-            Notes
-          </Text>
-          <TextInput
-            style={{
-              backgroundColor: "#EEE",
-              height: dimensions.height / 3,
-              width: dimensions.width / 1.15,
-              borderRadius: 15,
-              padding: 15,
-              textAlignVertical: "top",
-              fontSize: 16,
-            }}
-            // value={"demo"}
-          />
-        </View>
+            <Text
+              style={{
+                paddingVertical: 10,
+                fontSize: 17,
+                textAlign: "center",
+                fontWeight: "bold",
+              }}
+            >
+              Notes
+            </Text>
+            <TextInput
+              style={{
+                backgroundColor: "#EEE",
+                height: dimensions.height / 3,
+                width: dimensions.width / 1.15,
+                borderRadius: 15,
+                padding: 15,
+                textAlignVertical: "top",
+                fontSize: 17,
+              }}
+              value={
+                this.state.items[this.state.selectedDate] &&
+                this.state.items[this.state.selectedDate]
+              }
+              onChangeText={(text) => {
+                if (!this.state.items[this.state.selectedDate]) {
+                  this.state.items[this.state.selectedDate] = text;
+                } else {
+                  this.state.items[this.state.selectedDate] = text;
+                }
+                this.setState({ items: this.state.items });
+              }}
+            />
+          </View>
+        </ScrollView>
       </KeyboardAvoidingView>
     );
   }
